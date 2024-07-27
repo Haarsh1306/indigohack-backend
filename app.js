4692d8f3f6f4630d5fd81ceda817e4df5b5c3306
Harsh Verma <hv41096@gmail.com>
2024-07-27 22:30:23 +0530

    cors added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const { sendOTP } = require('./otpSender');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
