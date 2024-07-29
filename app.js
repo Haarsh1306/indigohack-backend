0e63886cb840b6ea6c7885ca0ac8ccb15f5ad57b
Harsh Verma <hv41096@gmail.com>
2024-07-29 18:52:02 +0530

    changes

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const authenticateToken = require('./middlewares/authenticate');
-const { startConsumer } = require('./kafka/consumer');
-const { connectProducer, sendEmailMessage } = require('./kafka/producer');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const authenticateToken = require("./middlewares/authenticate");
+const { startConsumer } = require("./kafka/consumer");
+const { connectProducer, sendEmailMessage } = require("./kafka/producer");
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/v1/user', require('./routes/user'));
-app.use('/api/v1/flight', require('./routes/flight'));
+app.use("/api/v1/user", require("./routes/user"));
+app.use("/api/v1/flight", require("./routes/flight"));
 
-app.get('/api/v1/getme', authenticateToken, (req, res) => {
-    res.json({ message: 'Authenticated', userId : req.user.userId, userEmail : req.user.email, userName : req.user.name });
+app.get("/api/v1/getme", authenticateToken, (req, res) => {
+
+  res.json({
+    message: "Authenticated",
+    userId: req.user.userId,
+    userEmail: req.user.userEmail,
+    userName: req.user.userName,
+  });
 });
 
 startConsumer().catch(console.error);
 connectProducer().catch(console.error);
 
-app.listen(3000, () => {   
-    console.log('Server is running on http://localhost:3000');
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
